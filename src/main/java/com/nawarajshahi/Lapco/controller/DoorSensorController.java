6ba38e4bb2fd3552a2aa528953b3576a3ceb4b76
Nawaraj Shahi <nawaraj.shahi@outlook.com>
2021-02-20 20:51:30 -0500

    created full functional createDoorRead() method.

--- src/main/java/com/nawarajshahi/Lapco/controller/DoorSensorController.java
+++ src/main/java/com/nawarajshahi/Lapco/controller/DoorSensorController.java
@@ -1,9 +1,22 @@
 package com.nawarajshahi.Lapco.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nawarajshahi.Lapco.Entity.CoinSensor;
+import com.nawarajshahi.Lapco.Entity.DoorSensor;
+import com.nawarajshahi.Lapco.service.DoorSensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restroom/{rest_id}")
 public class DoorSensorController {
+
+    @Autowired
+    DoorSensorService doorSensorService;
+
+    @RequestMapping(value = "/restrooms", method = RequestMethod.POST)
+    public ResponseEntity<?> createDoorRead(@PathVariable Long rest_id, @RequestBody DoorSensor doorSensor){
+        return new ResponseEntity<>(doorSensorService.createDoorRead(rest_id, doorSensor), HttpStatus.CREATED);
+    }
 }
